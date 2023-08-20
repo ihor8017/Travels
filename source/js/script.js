adc04f9bc4009591a5d4501b074e049b7a46edd4
Ihor Chyzh <djar80@gmail.com>
2023-08-20 15:56:59 -0500

    reviews

--- source/js/script.js
+++ source/js/script.js
@@ -4,9 +4,11 @@ import swiperHero from "./hero-slider.js";
 import playVideo from "./video.js";
 import upcomingSlider from "./upcoming-tours-slider.js";
 import trainingSlider from "./training-slider.js";
+import reviewsSlider from "./reviews-slider.js";
 
 toggleNavigation();
 swiperHero();
 playVideo();
 upcomingSlider();
 trainingSlider();
+reviewsSlider();
